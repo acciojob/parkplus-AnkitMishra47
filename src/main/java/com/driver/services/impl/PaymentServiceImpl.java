69e4f9456520572dedeafa02d7a366f1d60d3b2f
Ankit Mishra <ankitm17.2001@gmail.com>
2023-01-28 16:26:15 +0530

    Added the code

--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -29,6 +29,7 @@ public class PaymentServiceImpl implements PaymentService {
         }
 
         Payment payment = new Payment(true , getPaymentMode(mode) , reservation);
+        paymentRepository2.save(payment);
         reservation.setPayment(payment);
 
         reservationRepository2.save(reservation);
