49300423af2d72726f9c896ff1c7b721772e5983
Ankit Mishra <ankitm17.2001@gmail.com>
2023-01-28 17:03:09 +0530

    Added the code

--- src/main/java/com/driver/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/driver/services/impl/ReservationServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,68 +26,60 @@ public class ReservationServiceImpl implements ReservationService {
     ParkingLotRepository parkingLotRepository3;
     @Override
     public Reservation reserveSpot(Integer userId, Integer parkingLotId, Integer timeInHours, Integer numberOfWheels) throws Exception {
-        User user = getUser(userId);
+        try {
+            User user = getUser(userId);
 
-        if (user == null){
-            return null;
+            ParkingLot parkingLot = getParkingLot(parkingLotId);
+
+            List<Spot> spotList = parkingLot.getSpotList();
+
+            SpotType spotType = Spot.getSpotType(numberOfWheels);
+            Spot spot = null;
+
+            if (spotType == SpotType.TWO_WHEELER) {
+                /**
+                 * When spotType is TwoWheeler than all spot are going to work;
+                 */
+                spot = findSpot(spotList, timeInHours);
+
+            } else if (spotType == SpotType.FOUR_WHEELER) {
+                spotList = spotList.stream().filter(val -> val.getSpotType() != SpotType.TWO_WHEELER).collect(Collectors.toList());
+                spot = findSpot(spotList, timeInHours);
+            } else {
+                spotList = spotList.stream().filter(val -> val.getSpotType() == SpotType.OTHERS).collect(Collectors.toList());
+                spot = findSpot(spotList, timeInHours);
+            }
+
+            if (spot == null) {
+                throw new NullPointerException("Cannot make reservation");
+            }
+
+            spot.setOccupied(true);
+
+            Reservation reservation = new Reservation(timeInHours, user, spot);
+
+            List<Reservation> reservationList = spot.getReservationList();
+            reservationList.add(reservation);
+            spot.setReservationList(reservationList);
+
+            reservationList = user.getReservationList();
+            reservationList.add(reservation);
+            user.setReservationList(reservationList);
+
+            userRepository3.save(user);
+            spotRepository3.save(spot);
+
+            return reservation;
         }
-
-        ParkingLot  parkingLot  = getParkingLot(parkingLotId);
-
-        if (parkingLot == null){
-            return null;
+        catch (NoSuchElementException exception){
+            throw new Exception("Cannot make reservation");
         }
-
-        List<Spot>  spotList    = parkingLot.getSpotList();
-
-        SpotType spotType = Spot.getSpotType(numberOfWheels);
-        Spot spot = null;
-
-        if (spotType == SpotType.TWO_WHEELER)
-        {
-            /**
-             * When spotType is TwoWheeler than all spot are going to work;
-             */
-             spot = findSpot(spotList , timeInHours);
-
-        }
-        else if (spotType == SpotType.FOUR_WHEELER)
-        {
-            spotList = spotList.stream().filter(val -> val.getSpotType() != SpotType.TWO_WHEELER).collect(Collectors.toList());
-            spot = findSpot(spotList , timeInHours);
-        }
-        else
-        {
-            spotList = spotList.stream().filter(val -> val.getSpotType() == SpotType.OTHERS).collect(Collectors.toList());
-            spot = findSpot(spotList , timeInHours);
-        }
-
-        if (spot == null ){
-            throw new NullPointerException("Cannot make reservation");
-        }
-
-        spot.setOccupied(true);
-
-        Reservation reservation = new Reservation(timeInHours, user, spot);
-
-        List<Reservation> reservationList = spot.getReservationList();
-        reservationList.add(reservation);
-        spot.setReservationList(reservationList);
-
-        reservationList = user.getReservationList();
-        reservationList.add(reservation);
-        user.setReservationList(reservationList);
-
-        userRepository3.save(user);
-        spotRepository3.save(spot);
-
-        return reservation;
     }
-    public ParkingLot getParkingLot(int parkingId){
+    public ParkingLot getParkingLot(int parkingId) throws NoSuchElementException{
         return parkingLotRepository3.findById(parkingId).get();
     }
 
-    public User getUser(int userId){
+    public User getUser(int userId) throws NoSuchElementException {
         return userRepository3.findById(userId).get();
     }
 
