8bdd2696ffe6cdc15776ffc7a6bf6588bf84313d
Ankit Mishra <ankitm17.2001@gmail.com>
2023-01-28 17:04:39 +0530

    Added the code

--- src/main/java/com/driver/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/driver/services/impl/ReservationServiceImpl.java
@@ -72,7 +72,7 @@ public class ReservationServiceImpl implements ReservationService {
             return reservation;
         }
         catch (NoSuchElementException exception){
-            throw new Exception("Cannot make reservation");
+            throw new Exception("null");
         }
     }
     public ParkingLot getParkingLot(int parkingId) throws NoSuchElementException{
